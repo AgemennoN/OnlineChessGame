ba4f2cd154ee30eb576cfd52dfbf44c27137c79e
Egemen Ulucam <81033171+AgemennoN@users.noreply.github.com>
2022-08-25 21:39:11 +0300

    Highlightning Tiles
    When hovering with mouse the tiles get highlighted.

--- OnlineChessGame/Assets/Scripts/ChessBoard.cs
+++ OnlineChessGame/Assets/Scripts/ChessBoard.cs
@@ -15,12 +15,56 @@ public class ChessBoard : MonoBehaviour
     private const int TILE_COUNT_Y = 8;
     private const float zOffset = 0.1f;
     private GameObject[,] tiles;
+    private Camera currentCamera;
+    private Vector2Int currentHover;
 
     private void Awake()
     {
         GenerateChessBoard(tileSize, TILE_COUNT_X, TILE_COUNT_Y);
     }
 
+    private void Update()
+    {
+        if (!currentCamera)
+        {
+            currentCamera = Camera.main;
+            return;
+        }
+
+        RaycastHit info;
+        Ray ray = currentCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out info, 100, LayerMask.GetMask("Tile", "Hover")))
+        {
+            Vector2Int hitPosition = ReturnTileIndex(info.transform.gameObject);
+
+            // From not hovering to hovering
+            if (currentHover == -Vector2Int.one)
+            {
+                currentHover = hitPosition;
+                tiles[hitPosition.x, hitPosition.y].layer = LayerMask.NameToLayer("Hover");
+            }
+            
+            // From hovering one tile to hovering an other tile
+            if (currentHover != hitPosition)
+            {
+                tiles[currentHover.x, currentHover.y].layer = LayerMask.NameToLayer("Tile");
+                currentHover = hitPosition;
+                tiles[hitPosition.x, hitPosition.y].layer = LayerMask.NameToLayer("Hover");
+            }
+        }
+        else
+        {   // From hovering to not hovering on tiles 
+            if (currentHover != -Vector2Int.one)
+            {
+                tiles[currentHover.x, currentHover.y].layer = LayerMask.NameToLayer("Tile");
+                currentHover = -Vector2Int.one;
+            }
+        }
+    }
+
+
+    // Generate the Board 
     private void GenerateChessBoard(float tileSize, int tileCountX, int tileCountY)
     {
         tiles = new GameObject[tileCountX,tileCountY];
@@ -28,7 +72,6 @@ public class ChessBoard : MonoBehaviour
             for (int y = 0; y < tileCountY; y++)
                 tiles[x, y] = GenerateSingleTile(tileSize, x, y); 
     }
-
     private GameObject GenerateSingleTile(float tileSize, int x, int y)
     {
         GameObject tile = new GameObject(string.Format("X: {0}, Y: {1}", x, y));
@@ -52,7 +95,19 @@ public class ChessBoard : MonoBehaviour
         mesh.RecalculateNormals();
 
         tile.AddComponent<BoxCollider>();
+        tile.layer = LayerMask.NameToLayer("Tile");
 
         return tile;
+    }
+
+    // Operations
+    private Vector2Int ReturnTileIndex(GameObject hitInfo)
+    {
+        for (int x = 0; x < TILE_COUNT_X; x++)
+            for (int y = 0; y < TILE_COUNT_Y; y++)
+                if (tiles[x,y] == hitInfo)
+                    return new Vector2Int(x, y);
+
+        return -Vector2Int.one;
     }
 }
