6a83b82ad68ee82bb9b87502f7463b153c031995
Egemen Ulucam <81033171+AgemennoN@users.noreply.github.com>
2022-08-27 21:22:18 +0300

    All the Chess Pieces' movements have been adjusted.
    CS files of the pieces have been adjusted. Now the pieces move according to their types.

--- OnlineChessGame/Assets/Scripts/Pieces/Bishop.cs
+++ OnlineChessGame/Assets/Scripts/Pieces/Bishop.cs
@@ -1,6 +1,48 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Bishop : ChessPiece
 {
-  
+    public override List<Vector2Int> GetAvailableMoves(ref ChessPiece[,] board, int tileCountX, int tileCountY)
+    {
+        List<Vector2Int> r = new List<Vector2Int>();
+
+        List<Vector2Int> directionList = new List<Vector2Int>()
+        {
+            new Vector2Int(1, 1),
+            new Vector2Int(1, -1),
+            new Vector2Int(-1, -1),
+            new Vector2Int(-1, 1)
+        };
+
+        foreach (Vector2Int direction in directionList)
+        {
+            int distance = 1;
+            int checkTileX = currentX + (direction.x * distance);
+            int checkTileY = currentY + (direction.y * distance);
+
+            // If the tile in the direction is in the boundaries increase check distance in the direction
+            while (checkTileX < tileCountX && checkTileX >= 0 && checkTileY < tileCountY && checkTileY >= 0)
+            {
+                // If the tile is empty, it is available to move
+                if (board[checkTileX, checkTileY] == null)
+                {
+                    r.Add(new Vector2Int(checkTileX, checkTileY));
+                }
+                // If the tile is NOT empty
+                else
+                {
+                    // If the chess piece on the tile is an enemy piece, it is available to move
+                    if (board[checkTileX, checkTileY].team != team)
+                        r.Add(new Vector2Int(checkTileX, checkTileY));
+
+                    break;
+                }
+                distance++;
+                checkTileX = currentX + (direction.x * distance);
+                checkTileY = currentY + (direction.y * distance);
+            }
+        }
+        return r;
+    }
 }
--- OnlineChessGame/Assets/Scripts/Pieces/Knight.cs
+++ OnlineChessGame/Assets/Scripts/Pieces/Knight.cs
@@ -1,6 +1,46 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Knight : ChessPiece
 {
+    public override List<Vector2Int> GetAvailableMoves(ref ChessPiece[,] board, int tileCountX, int tileCountY)
+    {
+        List<Vector2Int> r = new List<Vector2Int>();
 
+        List<Vector2Int> directionList = new List<Vector2Int>()
+        {
+            new Vector2Int(1, 2),
+            new Vector2Int(2, 1),
+            new Vector2Int(2, -1),
+            new Vector2Int(1, -2),
+            new Vector2Int(-1, -2),
+            new Vector2Int(-2, -1),
+            new Vector2Int(-2, 1),
+            new Vector2Int(-1, 2)
+        };
+
+        foreach (Vector2Int direction in directionList)
+        {
+            int checkTileX = currentX + direction.x;
+            int checkTileY = currentY + direction.y;
+
+            // Checks if the tile is in the boundaries
+            if (checkTileX < tileCountX && checkTileX >= 0 && checkTileY < tileCountY && checkTileY >= 0)
+            {
+                // If the tile is empty, it is available to move
+                if (board[checkTileX, checkTileY] == null)
+                {
+                    r.Add(new Vector2Int(checkTileX, checkTileY));
+                }
+                // If the tile is NOT empty
+                else
+                {
+                    // If the chess piece on the tile is an enemy piece, it is available to move
+                    if (board[checkTileX, checkTileY].team != team)
+                        r.Add(new Vector2Int(checkTileX, checkTileY));
+                }
+            }
+        }
+        return r;
+    }
 }
